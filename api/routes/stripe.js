d94b8f7b212aa110829c7d92a08d59c33fe58e0c
ryankuclux <ryanfebriansyah0627@gmail.com>
2024-12-29 22:47:12 +0700

    add payment gateway

---
+++ api/routes/stripe.js
@@ -0,0 +1,24 @@
+import express from "express";
+import Stripe from "stripe";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const router = express.Router();
+const stripe = new Stripe(process.env.STRIPE_KEY);
+
+router.post("/payment", async (req, res) => {
+  try {
+    const charge = await stripe.charges.create({
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: "eur",
+    });
+    res.status(200).json(charge);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+export default router;
--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -1,101 +1,141 @@
-import React, { useContext, useState, } from "react";
-import "./reserve.css"
+import React, { useContext, useEffect, useState } from "react";
+import "./reserve.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 import useFetch from "../../hooks/useFetch";
 import { SearchContext } from "../../context/SearchContext";
 import axios from "axios";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+import StripeCheckout from "react-stripe-checkout";
 
-const Reserve = ({setOpen, hotelId}) => {
-    const [selectedRooms, setSelectedRooms] = useState([])
-    const { data, loading, error } = useFetch(`/api/hotels/room/${hotelId}`)
-    const { dates } = useContext(SearchContext)
+const KEY = "pk_test_51QbMGcJIZEdecxEeHYkDS9M5QBIrvMn0K3STu180mmKTBouFsZFjMxluLOq2JXIe2DkQraNRZkXWs8M8H53N6KKJ001sELoFXj"; // Publishable Key
 
-    const getDatesInRange = (startDate, endDate) => {
-        const start = new Date(startDate)
-        const end = new Date(endDate)
+const Reserve = ({ setOpen, hotelId }) => {
+  const [selectedRooms, setSelectedRooms] = useState([]);
+  const { data, loading, error } = useFetch(`/api/hotels/room/${hotelId}`);
+  const { dates } = useContext(SearchContext);
 
-        const date = new Date(start.getTime())
+  const getDatesInRange = (startDate, endDate) => {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
 
-        const dates = []
+    const date = new Date(start.getTime());
 
-        while (date <= end) {
-            dates.push(new Date(date).getTime())
-            date.setDate(date.getDate() +1)
-        }
+    const dates = [];
 
-        return dates
+    while (date <= end) {
+      dates.push(new Date(date).getTime());
+      date.setDate(date.getDate() + 1);
     }
 
-    const allDates = getDatesInRange(dates[0].startDate, dates[0].endDate)
+    return dates;
+  };
 
-    const isAvailable = (roomNumber) => {
-        const isFound = roomNumber.unavailableDates.some((date) => 
-            allDates.includes(new Date(date).getTime())
-        )
+  const allDates = getDatesInRange(dates[0].startDate, dates[0].endDate);
 
-        return !isFound
+  const isAvailable = (roomNumber) => {
+    const isFound = roomNumber.unavailableDates.some((date) =>
+      allDates.includes(new Date(date).getTime())
+    );
+
+    return !isFound;
+  };
+
+  const handleSelect = (e) => {
+    const checked = e.target.checked;
+    const value = e.target.value;
+
+    setSelectedRooms(
+      checked
+        ? [...selectedRooms, value]
+        : selectedRooms.filter((item) => item !== value)
+    );
+  };
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (selectedRooms.length === 0) {
+      alert("Please select at least one room.");
     }
+  };
 
-    const handleSelect = (e) => {
-        const checked = e.target.checked
-        const value = e.target.value
+  const onToken = async (token) => {
+    try {
+      await Promise.all(
+        selectedRooms.map((roomId) => {
+          const res = axios.put(`/api/rooms/availability/${roomId}`, {
+            dates: allDates,
+          });
+          return res.data;
+        })
+      );
 
-        setSelectedRooms (
-            checked ? [...selectedRooms, value] : selectedRooms.filter((item) => item !== value)
-        )
+      // Kirim token ke backend untuk diproses
+      const response = await axios.post("/api/checkout/payment", {
+        tokenId: token.id,
+        amount: 20000, // Ganti dengan jumlah yang seharusnya
+      });
+
+      console.log(response.data); // Tangani response dari backend
+
+      setOpen(false);
+      navigate("/");
+    } catch (err) {
+      console.error(err); // Tangani error dari backend, misalnya tampilkan pesan error ke user
     }
+  };
 
-    const navigate = useNavigate()
-
-    const handleClick = async () => {
-        try {
-            await Promise.all(
-                selectedRooms.map((roomId) => {
-                    const res = axios.put(`/api/rooms/availability/${roomId}`, {
-                        dates: allDates
-                    })
-
-                    return res.data
-                })
-            )
-
-            setOpen(false)
-            navigate("/")
-            
-        } catch (err) {}
-    }
-
-    return (
-        <div className="reserve">
-            <div className="rContainer">
-                <FontAwesomeIcon icon={faCircleXmark} className="rClose" onClick={ () => setOpen(false) } />
-                    <span>Select your rooms:</span>
-                    {data.map((item) => (
-                        <div className="rItem" key={item._id}>
-                            <div className="rItemInfo">
-                                <div className="rTitle">{item.title}</div>
-                                <div className="rDesc">{item.desc}</div>
-                                <div className="rMax">
-                                    Max people: <b>{item.maxPeople}</b>
-                                </div>
-                                <div className="rPrice">{item.price}</div>
-                            </div>
-                            <div className="rSelectRooms">
-                            {item.roomNumbers.map((roomNumber) => (
-                                <div className="room" key={roomNumber._id}>
-                                    <label>{roomNumber.number}</label>
-                                    <input type="checkbox" value={roomNumber._id } onChange={handleSelect} disabled={!isAvailable(roomNumber)} />
-                                </div>
-                            ))}
-                            </div>
-                        </div>
-                    ))}
-                    <button onClick={handleClick} className="rButton">Reserve Now!</button>
+  return (
+    <div className="reserve">
+      <div className="rContainer">
+        <FontAwesomeIcon
+          icon={faCircleXmark}
+          className="rClose"
+          onClick={() => setOpen(false)}
+        />
+        <span>Select your rooms:</span>
+        {data.map((item) => (
+          <div className="rItem" key={item._id}>
+            <div className="rItemInfo">
+              <div className="rTitle">{item.title}</div>
+              <div className="rDesc">{item.desc}</div>
+              <div className="rMax">
+                Max people: <b>{item.maxPeople}</b>
+              </div>
+              <div className="rPrice">{item.price}</div>
             </div>
-        </div>
-    )
-}
+            <div className="rSelectRooms">
+              {item.roomNumbers.map((roomNumber) => (
+                <div className="room" key={roomNumber._id}>
+                  <label>{roomNumber.number}</label>
+                  <input
+                    type="checkbox"
+                    value={roomNumber._id}
+                    onChange={handleSelect}
+                    disabled={!isAvailable(roomNumber)}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+        <StripeCheckout
+          name="name"
+          billingAddress
+          shippingAddress
+          description="desc"
+          amount={20000} // Ganti dengan jumlah yang seharusnya
+          token={onToken}
+          stripeKey={KEY}
+        >
+          <button onClick={handleClick} className="rButton">
+            Reserve Now!
+          </button>
+        </StripeCheckout>
+      </div>
+    </div>
+  );
+};
 
-export default Reserve
+export default Reserve;
